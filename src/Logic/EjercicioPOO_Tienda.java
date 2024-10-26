be940ed828bdbb0fff87bbf34b12b21152503899
Alejandro Duque <alejomudu@hotmail.com>
2024-10-26 15:37:59 -0500

    Subir carpetas primera vez

---
+++ src/Logic/EjercicioPOO_Tienda.java
@@ -0,0 +1,12 @@
+package Logic;
+
+public class EjercicioPOO_Tienda {
+
+    
+    public static void main(String[] args) {
+        
+        // TODO code application logic here
+        
+    }
+    
+}
---
+++ src/Logic/Persona.java
@@ -0,0 +1,58 @@
+package Logic;
+
+public abstract class Persona {
+    
+    private String nombre;
+    
+    private String apellido;
+    
+    private String documento;
+    
+    private String edad;
+
+    public Persona() {
+        
+    }
+
+    public Persona(String nombre, String apellido, String documento, String edad) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.documento = documento;
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDocumento() {
+        return documento;
+    }
+
+    public void setDocumento(String documento) {
+        this.documento = documento;
+    }
+
+    public String getEdad() {
+        return edad;
+    }
+
+    public void setEdad(String edad) {
+        this.edad = edad;
+    }
+    
+       
+    
+}
